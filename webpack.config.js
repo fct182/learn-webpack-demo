5dad961f74de77b43c5df5b69ddb5f016b30be2a
fct <CHANGTAO.FU@HAND-CHINA.COM>
2023-05-10 09:31:20 +0800

    feat: 增加对image图片文件打包

--- webpack.config.js
+++ webpack.config.js
@@ -5,5 +5,17 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.(jpg|png|jpeg|svg|gif)$/,
+        type: 'asset/resource'
+      }
+    ]
   }
 }
