360425980150df8ab176da330c7460dc76cbf618
fct <CHANGTAO.FU@HAND-CHINA.COM>
2023-05-10 11:40:07 +0800

    feat: webpack多入口配置、配置模板html

--- src/index.js
+++ src/index.js
@@ -5,23 +5,28 @@ import demoImg from './assets/img/demo.jpg';
 import jsonData from './assets/json/demo.json';
 
 // csv
-import csvData from "./assets/test.csv";
+import csvData from './assets/test.csv';
 // xml
-import xmlData from "./assets/test.xml";
+import xmlData from './assets/test.xml';
+
+import printMe from './print';
 
 function component() {
   const element = document.createElement('div');
 
   // lodash（目前通过一个 script 引入）对于执行这一行是必需的
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-
   element.classList.add('hello');
 
   const img = new Image();
   img.src = demoImg;
   img.className = 'img';
-
   element.appendChild(img);
+
+  const btn = document.createElement('button');
+  btn.innerHTML = 'click me';
+  btn.onclick = printMe;
+  element.appendChild(btn);
 
   return element;
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,21 @@
 const path = require('path');
+// HTML模板中 BASE_URL 变量的设置
+const { DefinePlugin } = require("webpack");
+
+const htmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: './src/index.js',
+  // entry: './src/index.js',
+  entry: {
+    // 多入口
+    index: './src/index.js',
+    print: './src/print.js'
+  },
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js'
+    // filename: 'bundle.js'
+    // 多入口配置
+    filename: '[name].bundle.js'
   },
   module: {
     rules: [
@@ -29,5 +40,14 @@ module.exports = {
         use: 'xml-loader'
       }
     ]
-  }
+  },
+  plugins: [
+    new DefinePlugin({
+      BASE_URL: "'./'"
+    }),
+    new htmlWebpackPlugin({
+      title: '管理输出',
+      template: './public/index.html'
+    })
+  ]
 }
